da703462d21dcc9eb60cbe176f60fa75ac59a7de
Harisaikumarbathula <harisaikumar745@gmail.com>
2025-02-22 11:44:57 +0530

    COMMIT

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import Navbar from "./navbar.jsx";
 import Form from "./form.jsx";
 import styled from "styled-components";
+import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
   const [count, setCount] = useState(0)
   return (
+    <>
     <div className="app"  >
       <Navbar />
       <div className="form-container">
       <Form />
       </div>
     </div>
+    </>
   )
 }
 
